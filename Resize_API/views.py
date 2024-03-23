fb802f9429c6dc9ea34b7cbc0a88908348981904
Videet <vinay.lahare00@gmail.com>
2024-03-23 17:16:51 +0530

    added nation code

--- Resize_API/views.py
+++ Resize_API/views.py
@@ -1,25 +1,26 @@
 from django.http import HttpResponse
 import requests
-from Resize_API import settings
+from Resize_API import settings, const
 import json
 
 def ready_home(request):
     return HttpResponse("This is Home Page")
 
-def send_msg(request, sendNumber, message):
+def send_msg(request, nation, sendNumber, message):
     try:
         auth = {
             "Authorization" : settings.TOKEN
         }
+        number = const.NATION[nation] + str(sendNumber)
         payload = {
             "messaging_product" : "whatsapp",
-            "to": sendNumber,
+            "to": int(number),
             "type" : "text",
             "text" : {"body" : message}
         }
         response = requests.post(settings.WA_URL, headers=auth, json=payload)
         ans = response.json()
-        return ans
+        return HttpResponse(f"Response {ans}")
     except Exception as e:
         HttpResponse(f"Failed due to {e}")
 
