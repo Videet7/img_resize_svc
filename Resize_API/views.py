c67cd3eef94992d31342a249e7e4bf9d0fa41adf
Videet <vinay.lahare00@gmail.com>
2024-03-25 01:40:11 +0530

    Modified WhatsAppWebhook

--- Resize_API/views.py
+++ Resize_API/views.py
@@ -6,15 +6,16 @@ import json
 def ready_home(request):
     return HttpResponse("This is Home Page")
 
-def send_msg(request, nation, sendNumber, message):
+def send_msg(request, nation=None, sendNumber=None, message=None):
     try:
         auth = {
             "Authorization" : settings.TOKEN
         }
-        number = const.NATION[nation] + str(sendNumber)
+        if nation:
+            number = const.NATION[nation] + str(sendNumber)
         payload = {
             "messaging_product" : "whatsapp",
-            "to": int(number),
+            "to": int(number) if number else sendNumber,
             "type" : "text",
             "text" : {"body" : message}
         }
@@ -22,6 +23,8 @@ def send_msg(request, nation, sendNumber, message):
         ans = response.json()
         return HttpResponse(f"Response {ans}")
     except Exception as e:
+        if isinstance(e, KeyError):
+            send_msg(request=None,nation='ind',sendNumber='9307103565', message=f'Error occurred due to {str(e)}')
         HttpResponse(f"Failed due to {e}")
 
 def whatsAppWebhook(request):
@@ -38,7 +41,16 @@ def whatsAppWebhook(request):
             
         if request.method == "POST":
             data = json.loads(request.body)
+            if 'object' in data and 'entry' in data and  data['object'] == 'whatsapp_business_account':
+                try:
+                    for entry in data['entry']:
+                        ph_no = entry['changes'][0]['value']['metadata']['display_phone_number']
+                        text = entry['changes'][0]['value']['messages'][0]['text']['body']
 
+                        revert = f"Thank You for you message -- {text}"
+                        send_msg(request,'ind', ph_no, revert)
+                except Exception as e:
+                    send_msg(request,'ind','9307103565', f'Error occurred due to {str(e)}')
 
     except Exception as e:
         HttpResponse(f"Failed due to {e}")   
